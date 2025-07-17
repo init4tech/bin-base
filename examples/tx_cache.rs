4444f2694518311ff70884407540c7376dab7c55
evalir <e@evalir.xyz>
2025-07-17 12:47:44 -0400

    chore: disabling example as we don't have a tnet for now

--- examples/tx_cache.rs
+++ examples/tx_cache.rs
@@ -5,19 +5,19 @@ use signet_tx_cache::client::TxCache;
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
-    let cfg = OAuthConfig::from_env()?;
-    let authenticator = cfg.authenticator();
-    let token = authenticator.token();
+    // let cfg = OAuthConfig::from_env()?;
+    // let authenticator = cfg.authenticator();
+    // let token = authenticator.token();
 
-    let _jh = authenticator.spawn();
+    // let _jh = authenticator.spawn();
 
-    tokio::time::sleep(std::time::Duration::from_secs(5)).await;
+    // tokio::time::sleep(std::time::Duration::from_secs(5)).await;
 
-    let tx_cache = BuilderTxCache::new(TxCache::pecorino(), token);
+    // let tx_cache = BuilderTxCache::new(TxCache::pecorino(), token);
 
-    let bundles = tx_cache.get_bundles().await?;
+    // let bundles = tx_cache.get_bundles().await?;
 
-    println!("Bundles: {bundles:#?}");
+    // println!("Bundles: {bundles:#?}");
 
     Ok(())
 }
