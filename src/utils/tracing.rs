8ac3c906ee31fe947048e369469879b46a2ae60e
Fraser Hutchison <190532+Fraser999@users.noreply.github.com>
2026-02-27 18:06:03 +0000

    support including file and line number in log lines

--- src/utils/tracing.rs
+++ src/utils/tracing.rs
@@ -5,28 +5,36 @@ use crate::utils::{
 use tracing_subscriber::{filter::EnvFilter, layer::SubscriberExt, util::SubscriberInitExt, Layer};
 
 const TRACING_LOG_JSON: &str = "TRACING_LOG_JSON";
+const TRACING_WITH_FILE_AND_LINE_NO: &str = "TRACING_WITH_FILE_AND_LINE_NO";
 
-/// Install a format layer based on the `TRACING_LOG_JSON` environment
-/// variable, and then install the register
+/// Install a format layer based on the `TRACING_LOG_JSON` and
+/// `TRACING_WITH_FILE_AND_LINE_NO` environment variables, and then install
+/// the registry.
 macro_rules! install_fmt {
-    (json @ $registry:ident, $filter:ident) => {{
+    (json @ $registry:ident, $filter:ident, $file_line:expr) => {{
         let fmt = tracing_subscriber::fmt::layer()
             .json()
             .with_span_list(true)
             .with_current_span(false)
+            .with_file($file_line)
+            .with_line_number($file_line)
             .with_filter($filter);
         $registry.with(fmt).init();
     }};
-    (log @ $registry:ident, $filter:ident) => {{
-        let fmt = tracing_subscriber::fmt::layer().with_filter($filter);
+    (log @ $registry:ident, $filter:ident, $file_line:expr) => {{
+        let fmt = tracing_subscriber::fmt::layer()
+            .with_file($file_line)
+            .with_line_number($file_line)
+            .with_filter($filter);
         $registry.with(fmt).init();
     }};
     ($registry:ident, $filter:ident) => {{
         let json = bool::from_env_var(TRACING_LOG_JSON).unwrap_or(false);
+        let file_line = bool::from_env_var(TRACING_WITH_FILE_AND_LINE_NO).unwrap_or(false);
         if json {
-            install_fmt!(json @ $registry, $filter);
+            install_fmt!(json @ $registry, $filter, file_line);
         } else {
-            install_fmt!(log @ $registry, $filter);
+            install_fmt!(log @ $registry, $filter, file_line);
         }
     }};
 }
@@ -40,8 +48,10 @@ macro_rules! install_fmt {
 ///
 /// ## Env Reads
 ///
-/// - `TRACING_LOG_JSON` - If set, will enable JSON logging.
-/// - As [`OtelConfig`] documentation for env var information.
+/// - `TRACING_LOG_JSON` - If set to a non-empty value, will enable JSON logging.
+/// - `TRACING_WITH_FILE_AND_LINE_NO` - If set to a non-empty value, will include file names and
+///   line numbers in tracing output.
+/// - See [`OtelConfig`] documentation for env var information.
 ///
 /// ## Panics
 ///
